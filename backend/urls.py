fc7d28bfd0abb948cd853a76ca4787822dcc4153
Adarsh Tayde <102159256+Adarsh-365@users.noreply.github.com>
2025-07-07 21:23:04 +0530

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index ,home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home , name = 'home'),
     path('main', index, name='index'),
 ]
