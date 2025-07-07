5707f6161ab208e1074f3a2e5accc4eed1a745db
Adarsh Tayde <102159256+Adarsh-365@users.noreply.github.com>
2025-07-07 21:15:40 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-c&5gw_hgzo!l@5p*3tij4b368+(dtpwev5_q%39r*2i3u-i!7w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    ''whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
