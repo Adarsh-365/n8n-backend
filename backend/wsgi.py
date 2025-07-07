62d859975439c76a6ed4d5d2c50aa48d44f94ac1
Adarsh Tayde <102159256+Adarsh-365@users.noreply.github.com>
2025-07-07 21:14:36 +0530

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+# application = get_wsgi_application()
+
+
+import os
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+# Initialize the Django WSGI application
 application = get_wsgi_application()
+
+# Wrap the application with WhiteNoise
+application = WhiteNoise(application)
+
+# Define app or handler for Vercel
+app = application  # Vercel expects this
